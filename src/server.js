9fb7b40ddc7b500b00b1f61f367ed2ef4e14bf3f
Artem <111749288+ArtemUah@users.noreply.github.com>
2024-06-27 08:33:40 +0300

    init project cors pino

--- src/server.js
+++ src/server.js
@@ -1,15 +1,32 @@
 import express from 'express';
+import pino from 'pino-http';
 import cors from 'cors';
+import env from './utils/env.js';
 
 const setupServer = () => {
     const app = express();
 
-    app.use(cors);
-    const PORT = 3000;
+    const PORT = env('PORT');
+
+    app.use(cors());
+
+    app.use(
+        pino({
+          transport: {
+            target: 'pino-pretty',
+          },
+        }),
+      );
 
     app.get('/', (req, res)=>{
         res.json({
             message:'Hello world',
+        });
+    });
+
+    app.use('*', (req, res, next) => {
+        res.status(404).json({
+            message:"Not found"
         });
     });
 
